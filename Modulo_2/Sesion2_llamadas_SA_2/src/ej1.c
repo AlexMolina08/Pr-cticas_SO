dc74fe28a6389c5b2f7feea6b272a2de9285a310
AlexMolina08 <alexmolinaugr@gmail.com>
2021-01-23 18:42:58 +0100

    s2 finished , starting s3

--- Modulo_2/Sesion2_llamadas_SA_2/src/ej1.c
+++ Modulo_2/Sesion2_llamadas_SA_2/src/ej1.c
@@ -1,13 +1,17 @@
 /*
 tarea3.c
 Trabajo con llamadas al sistema del Sistema de Archivos ''POSIX 2.10 compliant''
-Este programa fuente est� pensado para que se cree primero un programa con la parte
+Este programa fuente está pensado para que se cree primero un programa con la parte
  de CREACION DE ARCHIVOS y se haga un ls -l para fijarnos en los permisos y entender
  la llamada umask.
 En segundo lugar (una vez creados los archivos) hay que crear un segundo programa
  con la parte de CAMBIO DE PERMISOS para comprender el cambio de permisos relativo
  a los permisos que actualmente tiene un archivo frente a un establecimiento de permisos
  absoluto.
+
+
+
+
 */
 
 #include<sys/types.h>	//Primitive system data types for abstraction of implementation-dependent data types.
@@ -25,13 +29,18 @@ int fd1,fd2;
 struct stat atributos;
 
 //CREACION DE ARCHIVOS
+
+//crea archivo1 si no existe con permisos rw--wxrw-
+// abre con permisos de escritura
 if( (fd1=open("archivo1",O_CREAT|O_TRUNC|O_WRONLY,S_IRGRP|S_IWGRP|S_IXGRP))<0) {
 	printf("\nError %d en open(archivo1,...)",errno);
 	perror("\nError en open");
 	exit(EXIT_FAILURE);
 }
 
-umask(0);
+umask(0); // establecemos umask = 000
+
+// crea archivo 2 si no existe con permisos rw--wxrw-
 if( (fd2=open("archivo2",O_CREAT|O_TRUNC|O_WRONLY,S_IRGRP|S_IWGRP|S_IXGRP))<0) {
 	printf("\nError %d en open(archivo2,...)",errno);
 	perror("\nError en open");
@@ -39,15 +48,27 @@ if( (fd2=open("archivo2",O_CREAT|O_TRUNC|O_WRONLY,S_IRGRP|S_IWGRP|S_IXGRP))<0) {
 }
 
 //CAMBIO DE PERMISOS
+// llamada a stat para obtener estructura stat en atributos
 if(stat("archivo1",&atributos) < 0) {
 	printf("\nError al intentar acceder a los atributos de archivo1");
 	perror("\nError en lstat");
 	exit(EXIT_FAILURE);
 }
+
+// en atributos.st_mode están los permisos del archivo (campo de bits)
+// los permisos son 666 , ya que no hay mask 
+
+
+
+// elimina permisos de x a g y activa el bit especial
+// S_ISGID (grupo que ejecuta es como owner)
+
 if(chmod("archivo1", (atributos.st_mode & ~S_IXGRP) | S_ISGID) < 0) {
 	perror("\nError en chmod para archivo1");
 	exit(EXIT_FAILURE);
 }
+
+//rwx rw- r--
 if(chmod("archivo2",S_IRWXU | S_IRGRP | S_IWGRP | S_IROTH) < 0) {
 	perror("\nError en chmod para archivo2");
 	exit(EXIT_FAILURE);
